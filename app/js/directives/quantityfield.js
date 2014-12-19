f018469edf630c03c1c97a5e1241741904d982a9
Kyle Olson <kolson@four51.com>
2014-12-19 12:43:01 -0600

    Start of 13984

--- app/js/directives/quantityfield.js
+++ app/js/directives/quantityfield.js
@@ -67,12 +67,16 @@ four51.app.directive('quantityfield', ['$451', 'ProductDisplayService', function
 					//console.log('variant not selected can\'t check qty available'); //in vboss the user may select the qty before the variant. we may have to change when this gets called so inventory available can be re validated if the variant is chnaged based on a selection spec. It's probably not a big deal since the api will check inventory available on adding to order.
 				}
 				else{
-					var qtyAvail = (product.IsVariantLevelInventory ? variant.QuantityAvailable : product.QuantityAvailable) + (lineItem.OriginalQuantity || 0);
+					var qtyAvail = product.IsVariantLevelInventory ? variant.QuantityAvailable : lineItem.Product.OrderLimit ? (lineItem.Product.OrderLimit + (lineItem.OriginalQuantity || 0)) : (lineItem.QuantityAvailable + (lineItem.OriginalQuantity || 0));
 
 					if(qtyAvail < value && product.AllowExceedInventory == false){
 						scope.lineitem.qtyError = "cannot exceed the Quantity Available of " +  qtyAvail;
 						scope.valid = false;
 					}
+                    if (product.OrderLimit && qtyAvail < value) {
+                        scope.lineitem.qtyError = "cannot exceed the the maximum order quantity of " +  qtyAvail;
+                        scope.valid = false;
+                    }
 				}
 				if(scope.valid)
 					scope.lineitem.qtyError = null;
--- app/js/services/productService.js
+++ app/js/services/productService.js
@@ -7,66 +7,74 @@ four51.app.factory('Product', ['$resource', '$451', 'Security', 'User', function
 	}
 
 	function _extend(product) {
-		product.ViewName = product.ViewName || 'default';
-		angular.forEach(product.Specs, function(spec) {
-			if (spec.ControlType == 'File' && spec.File && spec.File.Url.indexOf('auth') == -1)
-				spec.File.Url += "&auth=" + Security.auth();
-		});
+        User.get(function(user) {
+            product.ViewName = product.ViewName || 'default';
+            angular.forEach(product.Specs, function(spec) {
+                if (spec.ControlType == 'File' && spec.File && spec.File.Url.indexOf('auth') == -1)
+                    spec.File.Url += "&auth=" + Security.auth();
+            });
 
-		angular.forEach(product.StaticSpecGroups, function(group) {
-			angular.forEach(group.Specs, function(spec) {
-				if (spec.FileURL && spec.FileURL.indexOf('auth') == -1)
-					spec.FileURL += "&auth=" + Security.auth();
-			});
-		});
+            angular.forEach(product.StaticSpecGroups, function(group) {
+                angular.forEach(group.Specs, function(spec) {
+                    if (spec.FileURL && spec.FileURL.indexOf('auth') == -1)
+                        spec.FileURL += "&auth=" + Security.auth();
+                });
+            });
 
-        if(product.StaticSpecGroups){
-            product.StaticSpecLength = 0;
-            product.StaticSpecGroups.EvenSpecGroups = [];
-            product.StaticSpecGroups.OddSpecGroups = [];
-            angular.forEach(product.StaticSpecGroups, function(g){
-                var visible = false;
-                for (var i in g.Specs) {
-                    if (g.Specs[i].VisibleToCustomer) {
-                        visible = true;
+            if(product.StaticSpecGroups){
+                product.StaticSpecLength = 0;
+                product.StaticSpecGroups.EvenSpecGroups = [];
+                product.StaticSpecGroups.OddSpecGroups = [];
+                angular.forEach(product.StaticSpecGroups, function(g){
+                    var visible = false;
+                    for (var i in g.Specs) {
+                        if (g.Specs[i].VisibleToCustomer) {
+                            visible = true;
+                        }
+                    }
+                    if (visible) {
+                        product.StaticSpecGroups.EvenSpecGroups.length == product.StaticSpecGroups.OddSpecGroups.length ? product.StaticSpecGroups.EvenSpecGroups.push(g) : product.StaticSpecGroups.OddSpecGroups.push(g);
+                        product.StaticSpecLength++;
+                    }
+                });
+            }
+
+            // parse old tokens to retrieve their values
+            angular.forEach(product.Specs, function(spec) {
+                if (spec.DefaultValue && spec.DefaultValue == spec.Value) {
+                    var matches = spec.DefaultValue.match(/\[\[(.*?)\]\]/g);
+                    if (matches) {
+                        User.get(function (user) {
+                            angular.forEach(matches, function(token) {
+                                var fix = token.replace(/\[/g, '').replace(/\]/g, '').replace(/\[/g, '');
+                                var split = fix.split(".");
+                                var temp = null, value;
+                                for(var i = 0; i <= split.length - 1; i++) {
+                                    temp = temp ? temp[split[i]] : user[split[i]];
+                                }
+                                value = temp || lookupCustom(user, fix);
+                                spec.Value = spec.Value.replace(token, value).substr(0, spec.MaxLength);
+                                spec.DefaultValue = spec.DefaultValue.replace(token, value);
+                            });
+                        });
                     }
                 }
-                if (visible) {
-                    product.StaticSpecGroups.EvenSpecGroups.length == product.StaticSpecGroups.OddSpecGroups.length ? product.StaticSpecGroups.EvenSpecGroups.push(g) : product.StaticSpecGroups.OddSpecGroups.push(g);
-                    product.StaticSpecLength++;
+            });
+            function lookupCustom(user, token) {
+                var value = '';
+                angular.forEach(user.CustomFields, function(f) {
+                    if (f.Name == token)
+                        value = f.Value;
+                });
+                return value;
+            }
+
+            angular.forEach(user.CustomFields, function(field) {
+                if (field.Label == 'LimitProduct') {
+                    product.OrderLimit = +(field.DefaultValue || field.Value);
                 }
             });
-        }
-
-        // parse old tokens to retrieve their values
-        angular.forEach(product.Specs, function(spec) {
-            if (spec.DefaultValue && spec.DefaultValue == spec.Value) {
-                var matches = spec.DefaultValue.match(/\[\[(.*?)\]\]/g);
-                if (matches) {
-                    User.get(function (user) {
-                        angular.forEach(matches, function(token) {
-                            var fix = token.replace(/\[/g, '').replace(/\]/g, '').replace(/\[/g, '');
-                            var split = fix.split(".");
-                            var temp = null, value;
-                            for(var i = 0; i <= split.length - 1; i++) {
-                                temp = temp ? temp[split[i]] : user[split[i]];
-                            }
-                            value = temp || lookupCustom(user, fix);
-                            spec.Value = spec.Value.replace(token, value).substr(0, spec.MaxLength);
-                            spec.DefaultValue = spec.DefaultValue.replace(token, value);
-                        });
-                    });
-                }
-            }
         });
-		function lookupCustom(user, token) {
-			var value = '';
-			angular.forEach(user.CustomFields, function(f) {
-				if (f.Name == token)
-					value = f.Value;
-			});
-			return value;
-		}
 	}
 
      var _get = function(param, success, page, pagesize, searchTerm) {
